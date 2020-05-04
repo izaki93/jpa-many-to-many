caa27e3a38209c83563e90a0894154ac8731107b
Ibrahim Mohsen <i.Mohsen-c@tamkeentech.sa>
2020-05-04 19:49:12 +0300

    JPA / Hibernate Many to Many Mapping Example with Spring Boot

---
+++ src/main/java/com/example/jpa/model/Tag.java
@@ -0,0 +1,38 @@
+package com.example.jpa.model;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @NaturalId
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            },
+            mappedBy = "tags")
+    private Set<Post> posts = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
+}
